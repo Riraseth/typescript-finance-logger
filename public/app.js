acea68b6fa53d52aabf04467058bd59547160d57
Łukasz Zabuski <riraseth@gmail.com>
2020-05-20 01:05:56 +0200

    added classes examples

--- public/app.js
+++ public/app.js
@@ -1,12 +1,26 @@
 "use strict";
-var anchor = document.querySelector('a');
-if (anchor) {
-    console.log(anchor.href);
-}
-console.log(anchor.href);
+// classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return this.client + " owes $" + this.amount + " for " + this.details;
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Bob', 'Mushrooms', 42);
+var invTwo = new Invoice('John', 'Beer', 86);
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+invOne.client = 'Mario';
+invOne.amount = 400;
 //const form = document.querySelector('form')!;
 var form = document.querySelector('.new-item-form');
-console.log(form.children);
+// console.log(form.children);
 // inputs
 var type = document.querySelector('#type');
 var tofrom = document.querySelector('#tofrom');
@@ -14,5 +28,5 @@ var details = document.querySelector('#details');
 var amount = document.querySelector('#amount');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    // console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,34 @@
-const anchor = document.querySelector('a')!;
-if(anchor) {
-  console.log(anchor.href);
+// classes
+
+class Invoice {
+  client: string;
+  details: string;
+  amount: number;
+
+  constructor(c: string, d: string, a: number) {
+    this.client = c;
+    this.details = d;
+    this.amount = a;
+  }
+
+  format() {
+    return `${this.client} owes $${this.amount} for ${this.details}`;
+  }
 }
-console.log(anchor.href);
+
+const invOne = new Invoice('Bob', 'Mushrooms', 42);
+const invTwo = new Invoice('John', 'Beer', 86);
+
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invOne.client = 'Mario';
+invOne.amount = 400;
 
 //const form = document.querySelector('form')!;
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
+// console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
@@ -17,10 +39,5 @@ const amount = document.querySelector('#amount') as HTMLInputElement;
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
 
-  console.log(
-    type.value, 
-    tofrom.value, 
-    details.value, 
-    amount.valueAsNumber
-  );
+  // console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
